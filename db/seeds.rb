c7f79fcb2549652fa92f08f6c1b158c9ac7a3a58
Charlie Knight <williamchknight@gmail.com>
2020-12-13 12:09:20 -0800

    renders correctly but not passing tests due to bugs with gemfil. Re-cloning.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "yes", store: "Safeway")
+Coupon.create(coupon_code: "nope", store: "TNC")
